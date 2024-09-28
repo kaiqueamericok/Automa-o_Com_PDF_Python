cbb0d8c7efd4af948e982522768a9d21fd29cdb6
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2024-09-28 09:27:39 -0300

    Add files via upload

---
+++ 3-separando_pdf_por_paginas/pdf_operations_separar.py
@@ -0,0 +1,45 @@
+import PyPDF2 as pdf
+from PyPDF2 import PdfReader, PdfWriter
+import os
+
+
+def get_pdf_metadata(pdf_path):
+    with open(pdf_path, 'rb') as f: 
+        reader = PdfReader(f)
+        info = reader.metadata 
+    return info 
+
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as f: 
+        reader = PdfReader(f)
+        results = []
+        for i in range(0, len(reader.pages)):
+            selected_page = reader.pages[i]
+            text =  selected_page.extract_text()
+            results.append(text)
+        return ' '.join(results)
+
+def split_pdf(pdf_path):
+    with open(pdf_path, 'rb') as f: 
+        reader = PdfReader(f)
+        for page_num in range(0, len(reader.pages)):
+            selected_page = reader.pages[page_num]
+            writer = PdfWriter() 
+            writer.add_page(selected_page)
+            # print(os.path.split(pdf_path)[1])
+            filename = os.path.split(pdf_path)[1]
+            new_filename = f'files/{filename}_{page_num+1}.pdf'
+
+            with open(new_filename, 'wb') as out: 
+                writer.write(out) 
+            print(f'PDF criado com o nome: {new_filename}') 
+# 1- Buscando Dados e Metadados de um Arquivo PDf
+#print(get_pdf_metadata('files/test_pdf.pdf'))
+#print(get_pdf_metadata('files/exemplo.pdf'))
+#print(extract_text_from_pdf('files/test_pdf.pdf'))  
+#print(extract_text_from_pdf('files/.pdf'))
+
+# 2 - Dividindo PDF por página 
+split_pdf('files/exemplo.pdf')  
+
+
